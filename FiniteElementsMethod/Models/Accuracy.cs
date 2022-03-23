0734676f7f8010400ec43c4c26a939e50787dc8a
boorlakov <vladimir.boorlakov@gmail.com>
2022-03-23 12:14:19 +0700

    Add docs to Models folder & suppress null value

--- FiniteElementsMethod/Models/Accuracy.cs
+++ FiniteElementsMethod/Models/Accuracy.cs
@@ -1,8 +1,22 @@
 namespace FiniteElementsMethod.Models;
 
+/// <summary>
+/// Accuracy model for given essential info about solving slae & other iteration processes
+/// </summary>
 public class Accuracy
 {
+    /// <summary>
+    /// Maximum iterations that can be done by iteration process
+    /// </summary>
     public int MaxIter { get; init; }
+
+    /// <summary>
+    /// Parameter for checking relative residual status
+    /// </summary>
     public double Eps { get; init; }
+
+    /// <summary>
+    /// Parameter for checking stagnation status
+    /// </summary>
     public double Delta { get; init; }
 }
