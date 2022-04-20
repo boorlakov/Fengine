d474237e337d536d02d2650acfff95321e042810
boorlakov <vladimir.boorlakov@gmail.com>
2022-04-20 21:25:14 +0700

    Name fix

--- Fengine/LinAlg/GeneralOperations.cs
+++ Fengine/LinAlg/GeneralOperations.cs
@@ -18,42 +18,42 @@ public static class GeneralOperations
     }
 
     /// <summary>
-    ///     Multiplication matrix3Diag by vector
+    ///     Multiplication m by vector
     /// </summary>
-    /// <param name="matrix3Diag">Given matrix3Diag in 3-diagonal format</param>
+    /// <param name="m">Given matrix in 3-diagonal format</param>
     /// <param name="vec">Vector, represented by double values</param>
-    /// <returns>Multiplication result matrix3Diag by vector</returns>
-    public static double[] MatMul(Matrix3Diag matrix3Diag, double[] vec)
+    /// <returns>Multiplication result m by vector</returns>
+    public static double[] MatMul(Matrix3Diag m, double[] vec)
     {
         var res = new double[vec.Length];
 
         for (var i = 0; i < vec.Length; i++)
         {
-            res[i] = Dot(i, matrix3Diag, vec);
+            res[i] = Dot(i, m, vec);
         }
 
         return res;
     }
 
     /// <summary>
-    ///     Dot product of a specified row in matrix3Diag by vector
+    ///     Dot product of a specified row in m by vector
     /// </summary>
     /// <param name="i">Row, along which the product is produced</param>
-    /// <param name="matrix3Diag">3-diagonal matrix3Diag</param>
+    /// <param name="m">3-diagonal matrix</param>
     /// <param name="vec">Vector, represented by double values</param>
-    /// <returns>Dot product of a specified row i in matrix3Diag by vector</returns>
-    public static double Dot(int i, Matrix3Diag matrix3Diag, double[] vec)
+    /// <returns>Dot product of a specified row i in m by vector</returns>
+    public static double Dot(int i, Matrix3Diag m, double[] vec)
     {
-        var res = matrix3Diag.Data["center"][i] * vec[i];
+        var res = m.Data["center"][i] * vec[i];
 
-        if (i >= 0 && i < matrix3Diag.Size - 1)
+        if (i >= 0 && i < m.Size - 1)
         {
-            res += matrix3Diag.Data["upper"][i] * vec[i + 1];
+            res += m.Data["upper"][i] * vec[i + 1];
         }
 
-        if (i > 0 && i <= matrix3Diag.Size - 1)
+        if (i > 0 && i <= m.Size - 1)
         {
-            res += matrix3Diag.Data["lower"][i - 1] * vec[i - 1];
+            res += m.Data["lower"][i - 1] * vec[i - 1];
         }
 
         return res;
