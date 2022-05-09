8655648e3590fc2a0ea55a11d95ea570c772775a
boorlakov <vladimir.boorlakov@gmail.com>
2022-05-09 21:31:15 +0700

    Remove unnecessary folder

---
+++ Fengine.Backend/DataModels/Conditions/Initial.cs
@@ -0,0 +1,9 @@
+namespace Fengine.Backend.DataModels.Conditions;
+
+public class Initial
+{
+    /// <summary>
+    ///     Initial function. Represented by string
+    /// </summary>
+    public string Func { get; init; } = string.Empty;
+}
--- Fengine.Backend/DataModels/Conditions/Initial/TwoDim.cs
+++
@@ -1,9 +0,0 @@
-namespace Fengine.Backend.DataModels.Conditions.Initial;
-
-public class TwoDim
-{
-    /// <summary>
-    ///     Initial function on lower border. Represented by string
-    /// </summary>
-    public string InitialFunc { get; init; } = string.Empty;
-}
